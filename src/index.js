7d74286eb96b76b4d0a522ca33b091ce0fa6714d
vinitpulstya <vinitsharma9876@gmail.com>
2023-10-20 12:23:54 +0530

    disable strict mode, it was causing apis to be called twice

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { AppStateContextProvider } from "./services/app-state-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AppStateContextProvider>
       <App />
     </AppStateContextProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
